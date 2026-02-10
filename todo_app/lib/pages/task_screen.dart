e55d54d554d0f97899b1753ba980f7b1d95da5df
katharsis23 <daniklv2006@gmail.com>
2026-02-10 19:20:11 +0200

    Added homepage

--- todo_app/lib/pages/task_screen.dart
+++ todo_app/lib/pages/task_screen.dart
@@ -6,6 +6,6 @@ class TaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(appBar: AppBar(title: Text(taskName)));
   }
 }
